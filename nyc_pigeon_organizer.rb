78057b78dc81b9f51af313f919a39db6fcf5fef2
inherited-window-0818 <robt15@hotmail.com>
2019-11-11 05:10:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,7 +18,7 @@ def nyc_pigeon_organizer(data)
       data.each{|attributes, values|
         values{|values, array|
           array{|trait|
-            if trait
+            if trait == 
           }
         }
       }
