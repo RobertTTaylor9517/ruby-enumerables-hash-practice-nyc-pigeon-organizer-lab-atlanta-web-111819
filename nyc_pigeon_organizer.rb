c4753e307ff687454e3cabf78126721efee90521
inherited-window-0818 <robt15@hotmail.com>
2019-11-11 05:09:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,7 +15,9 @@ def nyc_pigeon_organizer(data)
   }
   pigeon_data.each{|names, values|
     values.each{|name_hash, array|
-      data.each{|attributes, 
+      data.each{|attributes, values|
+        values{|
+      }
     }
   }
 end
